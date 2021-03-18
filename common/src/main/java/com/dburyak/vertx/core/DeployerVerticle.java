a11121f383fdbcaae182adc3e387f40fb129f5ed
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-03-18 12:28:55 +0200

    WIP

--- common/src/main/java/com/dburyak/vertx/core/DeployerVerticle.java
+++ common/src/main/java/com/dburyak/vertx/core/DeployerVerticle.java
@@ -1,4 +1,11 @@
 package com.dburyak.vertx.core;
 
+import com.dburyak.vertx.core.deployment.spec.Deployment;
+
 public class DeployerVerticle extends Verticle {
+    private final Deployment deployment;
+
+    public DeployerVerticle(Deployment deployment) {
+        this.deployment = deployment;
+    }
 }
